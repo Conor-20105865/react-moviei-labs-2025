db549462b5f015b3b6631fcfec1f7bc56daee2e0
Conor <147147702+Conor-20105865@users.noreply.github.com>
2025-10-19 18:46:02 +0100

    Movie Details coming from API.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -1,14 +1,53 @@
 import React from "react";
-import Movie from "../movieCard/";
-import Grid from "@mui/material/Grid";
+import { Link } from "react-router";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const MovieList = (props) => {
-  let movieCards = props.movies.map((m) => (
-    <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-      <Movie key={m.id} movie={m} />
-    </Grid>
-  ));
-  return movieCards;
+const MovieCard = ({ movie }) => {
+  return (
+    <Card
+      sx={{
+        borderRadius: 2,
+        boxShadow: 3,
+        height: 500, 
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between", 
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="300"
+        image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div">
+          {movie.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {movie.vote_average} | {movie.release_date}
+        </Typography>
+        <Typography variant="body2" sx={{ mt: 1, mb: 2 }}>
+          {movie.overview.length > 100
+            ? movie.overview.slice(0, 100) + "..."
+            : movie.overview}
+        </Typography>
+
+        {}
+        <div style={{ marginTop: "auto", textAlign: "center" }}>
+          <Link to={`/movies/${movie.id}`}>
+            <Button variant="outlined" size="medium" color="primary">
+              More Info ...
+            </Button>
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default MovieList;
+export default MovieCard;
